cd526bc2805c9547848dd6f084e14b0ea7085184
wikk2207 <wiktoriajuliasalamon@gmail.com>
2019-11-04 20:05:32 +0100

    Validation added

--- resources/js/components/auth/loginpage.vue
+++ resources/js/components/auth/loginpage.vue
@@ -1,19 +1,84 @@
 <template>
 	<div class="login-form">
 		
-		<v-text-field label="e-mail" outlined></v-text-field>
-		<v-text-field label="hasło" outlined></v-text-field>
+		<v-text-field 
+			label="e-mail" 
+			outlined
+			v-model="input.email"
+			:rules="[rules.required, rules.email]"
+		></v-text-field>
+		<v-text-field 
+			label="hasło" 
+			outlined
+			v-model="input.password"
+			:append-icon="showPassword ? 'visibility' : 'visibility_off'"
+			:type="showPassword ? 'text' : 'password'"
+			:rules="[rules.required]"
+			@click:append="showPassword = !showPassword"
+		></v-text-field>
 		<a class="a-forgot-password"> Nie pamiętam hasła </a>
 		<div class="login-button">
-			<v-btn large>Zaloguj się</v-btn>
+			<v-btn large @click="login()">Zaloguj się</v-btn>
 		</div>
+		<v-snackbar
+			v-model="snackbar.show"
+		>
+		{{ text }}
+		<v-btn
+			color="pink"
+			text
+			@click="snackbar.show = false"
+		>
+			Zamknij
+		</v-btn>
+		</v-snackbar>
 		<a class="a-register">Zarejestruj się</a>
 	</div>
 </template>
 
 <script>
   export default {
-    name: "loginPage"
+	name: "loginPage",
+
+	data() {
+		return {
+			showPassword: false,
+			formIsEmpty: true,
+			formHasErrors: false,
+			input: {
+				email: "",
+				password: "",
+			},
+			snackbar: {
+				show: false,
+			},
+			text: 'Hello, I\'m a snackbar',
+			rules: {
+				required: value => {
+					return !!value || 'Pole nie może być puste.';
+				},
+				email: value => {
+					const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+					return pattern.test(value) || 'Nieprawidłowy e-mail.';
+          		},
+			   },
+		}
+	},
+	methods: {
+		login() {
+			const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			if(this.input.email == "" || this.input.password == "") {
+				this.text='Pola nie mogą być puste';
+				this.snackbar.show = true;
+			} else if (!pattern.test(this.input.email)) {
+				this.text='Wprowadzono nieprawidłowe dane';
+				this.snackbar.show = true;
+			} else {
+				this.text='Wszystko ok';
+				this.snackbar.show = true;
+			}
+		},
+	}
   }
 </script>
 
