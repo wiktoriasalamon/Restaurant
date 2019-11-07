fe9c29291094370faa8478f2b772938a62f30775
Jakub Pyka <pykajakubny@gmail.com>
2019-11-07 23:17:03 +0100

    delete dań, kategorii, stolików z warunkami

--- app/Http/Controllers/API/ApiDishCategoryController.php
+++ app/Http/Controllers/API/ApiDishCategoryController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dish;
 use App\Models\DishCategory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ApiDishCategoryController extends Controller
 {
@@ -17,5 +20,25 @@ class ApiDishCategoryController extends Controller
     public function index()
     {
         return response()->json(DishCategory::all());
+    }
+
+    /**
+     * @param DishCategory $dishCategory
+     * @return JsonResponse
+     */
+    public function delete(DishCategory $dishCategory)
+    {
+        try {
+            if($dishes = Dish::where('category_id', $dishCategory->id)->first()){
+                return response()->json('Przed usunięciem kategorii usuń dania', 419);
+            }
+            $dishCategory->delete();
+            return response()->json("Kategoria usunięta", 201);
+        } catch (\Exception $e) {
+            Log::notice("Error deleting data all:" . $e);
+            Log::notice("Error deleting data msg:" . $e->getMessage());
+            Log::notice("Error deleting data code:" . $e->getCode());
+            return response()->json('Wystąpił nieoczekiwany błąd', 500);
+        }
     }
 }
--- app/Http/Controllers/API/ApiDishController.php
+++ app/Http/Controllers/API/ApiDishController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Dish;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ApiDishController extends Controller
 {
@@ -17,5 +19,22 @@ class ApiDishController extends Controller
     public function index()
     {
         return response()->json(Dish::all()->load('category'));
+    }
+
+    /**
+     * @param Dish $dish
+     * @return JsonResponse
+     */
+    public function delete(Dish $dish)
+    {
+        try {
+            $dish->delete();
+            return response()->json("Danie usunięte", 201);
+        } catch (\Exception $e) {
+            Log::notice("Error deleting data all:" . $e);
+            Log::notice("Error deleting data msg:" . $e->getMessage());
+            Log::notice("Error deleting data code:" . $e->getCode());
+            return response()->json('Wystąpił nieoczekiwany błąd', 500);
+        }
     }
 }
