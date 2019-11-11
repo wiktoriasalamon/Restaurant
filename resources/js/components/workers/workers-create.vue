dfca3fbe593e26e9e13fd92e09c5d336c6b62ee4
jakubfilistynski <j.filistynski@bonasoft.pl>
2019-11-11 19:30:23 +0100

    formularz dodawania pracownika

--- resources/js/components/workers/workers-create.vue
+++ resources/js/components/workers/workers-create.vue
@@ -4,29 +4,34 @@
     <v-card-text>
       <v-container>
         <v-form
-          ref="form"
+            ref="form"
         >
-          <v-text-field label="Imię" :rules="[rules.required]" v-model="form.name"></v-text-field>
-          <v-text-field label="Nazwisko" :rules="[rules.required]" v-model="form.surname"></v-text-field>
-          <v-text-field label="Email" :rules="[rules.required, rules.emailRules]" v-model="form.email"></v-text-field>
-          <v-text-field label="Ulica" :rules="[rules.required]" v-model="form.address.street"></v-text-field>
+          <v-text-field :rules="[rules.required]" error-messages="errors.name" label="Imię"
+                        v-model="form.name"></v-text-field>
+          <v-text-field :rules="[rules.required]" error-messages="errors.surname" label="Nazwisko"
+                        v-model="form.surname"></v-text-field>
+          <v-text-field :rules="[rules.required, rules.emailRules]" error-messages="errors.email" label="Email"
+                        v-model="form.email"></v-text-field>
+          <v-text-field :rules="[rules.required]" error-messages="errors.address.street" label="Ulica"
+                        v-model="form.address.street"></v-text-field>
           <v-text-field label="Numer domu" :rules="[rules.required]"
-                        v-model="form.address.houseNumber"></v-text-field>
+                        error-messages="errors.address.houseNumber" v-model="form.address.houseNumber"></v-text-field>
           <v-text-field label="Numer apartamentu" :rules="[rules.required]"
+                        error-messages="errors.address.apartmentNumber"
                         v-model="form.address.apartmentNumber"></v-text-field>
-          <v-text-field label="Miasto" :rules="[rules.required]" v-model="form.address.city"></v-text-field>
+          <v-text-field :rules="[rules.required]" error-messages="errors.address.city" label="Miasto"
+                        v-model="form.address.city"></v-text-field>
           <v-text-field label="Kod pocztowy" :rules="[rules.required, rules.postCodeFormat]"
-                        v-model="form.address.postCode"></v-text-field>
-          <v-text-field label="Telefon" :rules="[rules.phoneMax12, rules.phoneNumber]"
-                        v-model="form.phone"></v-text-field>
-          <v-text-field label="Hasło" :rules="[rules.required]" v-model="form.name"></v-text-field>
-          <v-text-field label="Powtórz hasło" :rules="[rules.required]"
+                        error-messages="errors.address.postCode" v-model="form.address.postCode"></v-text-field>
+          <v-text-field :rules="[rules.phoneMax12]" error-messages="errors.phone"
+                        label="Telefon" v-model="form.phone"></v-text-field>
+          <v-text-field :rules="[rules.required, rules.passwordMax6]" error-messages="errors.password" label="Hasło"
                         v-model="form.name"></v-text-field>
         </v-form>
       </v-container>
     </v-card-text>
     <v-card-actions>
-      <v-btn>Anuluj</v-btn>
+      <v-btn @click="cancel">Anuluj</v-btn>
       <v-btn>Zapisz</v-btn>
     </v-card-actions>
   </v-card>
@@ -41,7 +46,8 @@
           required: value => !!value || "To pole jest wymagane",
           emailRules: v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Niepoprawny adres email',
           phoneMax12: value => value.length <= 12 || 'Numer telefonu powinien mieć mniej niż 13 znaków',
-          postCodeFormat: value => /^\d{2}-\d{3}$/.test(value) || 'Nieprawidłowy format kodu pocztowego'
+          postCodeFormat: value => /^\d{2}-\d{3}$/.test(value) || 'Nieprawidłowy format kodu pocztowego',
+          passwordMax6: value => value.length >= 6 || 'Hasło może mieć maksymalnie 6 znaków'
         },
         form: {
           name: '',
@@ -58,23 +64,37 @@
           password: '',
         },
         errors: {
-          name: '',
-          surname: '',
-          email: '',
+          name: [],
+          surname: [],
+          email: [],
           address: {
-            street: '',
-            houseNumber: '',
-            apartmentNumber: '',
-            city: '',
-            postCode: '',
+            street: [],
+            houseNumber: [],
+            apartmentNumber: [],
+            city: [],
+            postCode: [],
           },
-          phone: '',
-          password: '',
+          phone: [],
+          password: [],
         }
       };
     },
     methods: {
-
+      cancel() {
+        window.location.replace(route(worker.index));
+      },
+      save() {
+        let data = this.form;
+        data.address = JSON.stringify(data.address);
+        axios.post(route(), data).then(response => {
+          windows.location.replace(route(worker.index));
+        }).catch(error => {
+          let entries = Object.entries(error.response.data.messages);
+          for (let [key, value] of entries) {
+            this.error[key] = value;
+          }
+        });
+      }
     }
   }
 </script>
