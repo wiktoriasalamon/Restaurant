f6fa4649b103446c7c405bb63393339cca096e59
jakubfilistynski <j.filistynski@bonasoft.pl>
2019-11-13 01:31:01 +0100

    WIP: dish create

---
+++ resources/js/components/menu/create-dish.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-card-title>Dodawanie dania</v-card-title>
+    <v-card-text>
+      <v-form
+        ref="form">
+
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="cancel">Anuluj</v-btn>
+      <v-btn @click="save">Zapisz</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "create-dish",
+    data() {
+      return {
+
+      };
+    },
+    beforeMount() {
+
+    },
+    methods: {
+      cancel() {
+        window.location.replace(route('worker.index'));
+      },
+      save() {
+        if (this.$refs.form.validate()) {
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/menu/edit-dish.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    props: ['id'],
+    name: "edit-dish",
+    data() {
+      return {
+
+      };
+    },
+    beforeMount() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
