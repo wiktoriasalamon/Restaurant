c3452135491fb731ee0c044ea4ccd0b758be54de
jakubfilistynski <j.filistynski@bonasoft.pl>
2019-11-27 22:53:23 +0100

    podglad rezerwacji

--- resources/js/components/reservation/waiter-index-reservation.vue
+++ resources/js/components/reservation/waiter-index-reservation.vue
@@ -48,9 +48,6 @@
 						<td class="text-xs-left">{{ props.item.reservation.email}}</td>
 						<td class="text-xs-left">{{ props.item.status === 'current' ? "nadchodzący" : "archwilany"}}</td>
 						<td class="text-xs-center">
-							<v-icon @click="showItem(props.item.reservation.id)" small>
-								visibility
-							</v-icon>
 							<v-icon @click="deleteItem(props.item.reservation.id)" small>
 								delete
 							</v-icon>
@@ -122,9 +119,6 @@
         }).finally(()=>{
           this.loading = false
 				})
-      },
-      showItem(id) {
-        window.location.href = route('reservation.showWaiter', [id])
       },
       deleteItem(id) {
         axios.delete(route('api.reservation.delete', {
